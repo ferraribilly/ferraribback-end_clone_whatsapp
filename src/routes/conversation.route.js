af1301ca39fe931b168e96974bd8a75063a78081
ferraribilly <williamferrari52@gmail.com>
2025-07-15 02:09:43 -0300

    b

--- src/routes/conversation.route.js
+++ src/routes/conversation.route.js
@@ -3,7 +3,7 @@ import express from "express";
 import trimRequest from "trim-request";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import {
-  createGroup,
+  // createGroup,
   create_open_conversation,
   getConversations,
   
@@ -15,7 +15,7 @@ router
   .route("/")
   .post(trimRequest.all, authMiddleware, create_open_conversation);
 router.route("/").get(trimRequest.all, authMiddleware, getConversations);
-router.route("/group").post(trimRequest.all, authMiddleware, createGroup);
+// router.route("/group").post(trimRequest.all, authMiddleware, createGroup);
 
 
 export default router;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import trimRequest from "trim-request";
-import { searchUsers, getSentFriendRequests, getFriends, sendFriendRequests,  acceptFriendRequestByVehicle } from "../controllers/user.controller.js";
+import { searchUsers } from "../controllers/user.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 
 router.route("/").get(trimRequest.all, authMiddleware, searchUsers);
 
-router.route("/sent/:userId").get(trimRequest.all, authMiddleware, getSentFriendRequests);
-router.route("/:userId").get(trimRequest.all, authMiddleware, getFriends);
-router.route("/send-by-vehicle").post(trimRequest.all, authMiddleware, sendFriendRequests);
-router.route("/accept-by-vehicle").post(trimRequest.all, authMiddleware, acceptFriendRequestByVehicle);
+// router.route("/sent/:userId").get(trimRequest.all, authMiddleware, getSentFriendRequests);
+// router.route("/:userId").get(trimRequest.all, authMiddleware, getFriends);
+// router.route("/send-by-vehicle").post(trimRequest.all, authMiddleware, sendFriendRequests);
+// router.route("/accept-by-vehicle").post(trimRequest.all, authMiddleware, acceptFriendRequestByVehicle);
 
 export default router;
